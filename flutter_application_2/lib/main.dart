24f6c07dcfdbf3b547f8f5bbbbf98c14c17c91b3
hslee <dlgytjdsla@naver.com>
2022-10-05 21:09:29 +0900

    codingchef

---
+++ flutter_application_2/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '안녕',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('하이'),
+        centerTitle: true,
+        // leading: IconButton(
+        //   icon: Icon(Icons.menu),
+        //   onPressed: () => print('hi'),
+        // ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.mail),
+            onPressed: () => print('안녕'),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                  // backgroundImage: AssetImage(''),
+                  ),
+              accountName: Text('accountName'),
+              accountEmail: Text('accountEmail'),
+              onDetailsPressed: () => print('하이'),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundColor: Colors.red,
+                ),
+              ],
+            ),
+            ListTile(
+              leading: Icon(Icons.media_bluetooth_off),
+              title: Text('home'),
+              onTap: () => print('hi'),
+              trailing: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: const <Widget>[
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+          ],
+        ),
+      ),
+    );
+  }
+}
