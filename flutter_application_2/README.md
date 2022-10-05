f9867ec76b2ccbe1b2e0c0bb3e523573d3c5d5c1
hslee <dlgytjdsla@naver.com>
2022-10-06 01:10:20 +0900

    네이게이터

--- flutter_application_2/README.md
+++ flutter_application_2/README.md
@@ -51,3 +51,11 @@ appBar: AppBar(
  - of메소드는 현재 자신에서 부모까지 제일 가까운 scaffold를 찾는 메소드 이다.
  - Theme.of(context): 현재 자신에서 부모까지 제일 가까운 Theme를 찾는다.
  - scaffold.of(context)를 사용할 때 buildcontex기준으로 찾는데, 현재 위젯의 부모가 scaffold가 없다면, Builder함수를 선언하여 현재 위젯에서 Builder하여 상위 widget에 있는 scaffold를 참조 할 수 있다.
+
+# Router
+ - Scaffold는 하나의 페이지(위젯이다)임으로 모든 라우터에서 사용이 가능하다.
+ - 라우터 기능은 Stack기능이다.
+ - Navigaotr함수를 사용한다
+    -- push(context, route)
+        --- context는 위젯 트리의 위치정보를 근거하여 현재 화면상의 보여지는 페이지가 어떤페이지인지 확인하고 push함수가 이동하기 원하는 페이지를 쌓기 위함, Second Page를 쌓기위해 first Page의 정확한 위치를 알아야 하기 때문이다.
+        --- route는 이동할 페이지, MaterialPageRoute()
--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -13,38 +13,52 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: '안녕',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
+      home: const FirstPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('하이'),
-        centerTitle: true,
+        title: Text('First Page'),
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () => fluuterToast(),
-          child: Text('data'),
+          child: Text('Go to first Second page'),
+          onPressed: () => customNavigator(context, const SecondPage()),
         ),
       ),
     );
   }
 }
 
-void fluuterToast() {
-  Fluttertoast.showToast(
-    msg: 'msg',
-    gravity: ToastGravity.BOTTOM_LEFT,
-    backgroundColor: Colors.redAccent,
-    fontSize: 20.0,
-    textColor: Colors.black,
-    toastLength: Toast.LENGTH_SHORT,
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to first page'),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+    );
+  }
+}
+
+void customNavigator(BuildContext context, Widget pageName, [String? a]) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (BuildContext context) => pageName),
   );
 }
