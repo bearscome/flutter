f94f71df3c1c0a5c3215d053326c6988fd99599b
hslee <dlgytjdsla@naver.com>
2022-10-07 22:20:59 +0900

    onBoardingpage는 맨 처음 튜토리얼 같은 느낌의 페이지 이다.

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/ScreenB.dart';
-import 'package:flutter_application_1/ScreenA.dart';
-import 'package:flutter_application_1/ScreenC.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'onboarding.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,84 +8,40 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('button'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              TextButton(
-                onPressed: () {
-                  print('object');
-                },
-                onLongPress: () => print('object'),
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.red,
-                ),
-                child: const Text(
-                  'hi',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print('안녕');
-                },
-                child: Text('Elevated Button'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orangeAccent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+    return const MaterialApp(
+      home: OnBoardingPage(),
+    );
+  }
+}
+
+class Mypage extends StatelessWidget {
+  const Mypage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Main Screen',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const OnBoardingPage(),
                   ),
-                  elevation: 0.0,
-                ),
-              ),
-              OutlinedButton(
-                onPressed: () {
-                  print('OutlinedButton');
-                },
-                child: Text('OutlinedButton'),
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.green,
-                  side: const BorderSide(
-                    color: Colors.black87,
-                    width: 2.0,
-                  ),
-                ),
-              ),
-              TextButton.icon(
-                onPressed: () {
-                  print('TextButton.icon');
-                },
-                icon: const Icon(
-                  Icons.home,
-                  size: 30.0,
-                  color: Colors.white,
-                ),
-                label: const Text('Go to Home'),
-                style: TextButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    minimumSize: const Size(200, 50),
-                    backgroundColor: Colors.black87),
-              ),
-              ButtonBar(
-                alignment: MainAxisAlignment.center,
-                buttonPadding: EdgeInsets.all(20.0),
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: Text("hi"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('hi'),
-                  ),
-                ],
-              )
-            ],
-          ),
+                );
+              },
+              child: const Text('Go to onboarding screen'),
+            ),
+          ],
         ),
       ),
     );
---
+++ flutter_application_2/lib/onboarding.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: "Welcom",
+          body: 'This is FirstPage',
+          image: Image.asset('images/1.png'),
+          decoration: getPageDecoration(),
+        ),
+        PageViewModel(
+          title: "Welcom",
+          body: 'This is SecondPage',
+          image: Image.asset('images/2.png'),
+          decoration: getPageDecoration(),
+        ),
+        PageViewModel(
+          title: "Welcom",
+          body: 'This is ThirdPage',
+          image: Image.asset('images/3.png'),
+          decoration: getPageDecoration(),
+        ),
+      ],
+      done: const Text('done'),
+      onDone: () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const Mypage(),
+          ),
+        );
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (context) => const Mypage(),
+        //   ),
+        // );
+      },
+      next: const Icon(Icons.arrow_forward),
+      showSkipButton: true,
+      skip: const Text('skip'),
+      dotsDecorator: DotsDecorator(
+          color: Colors.cyan,
+          size: Size(10, 10),
+          activeSize: Size(22, 10),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          activeColor: Colors.pink),
+      curve: Curves.bounceOut,
+    );
+  }
+
+  PageDecoration getPageDecoration() {
+    return const PageDecoration(
+        titleTextStyle: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+        ),
+        bodyTextStyle: TextStyle(
+          fontSize: 18,
+          color: Colors.blue,
+        ),
+        imagePadding: EdgeInsets.only(
+          top: 40,
+        ),
+        pageColor: Colors.pink);
+  }
+}
