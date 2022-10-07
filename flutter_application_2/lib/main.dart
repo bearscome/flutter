e201f2c56f2c15b49cc6eb0d43bc98a329573402
hslee <dlgytjdsla@naver.com>
2022-10-07 21:13:04 +0900

    button

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -14,34 +14,85 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Builder(
-          builder: ((context) => Center(
-                child: ElevatedButton(
-                  child: Text('go to A'),
-                  onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: ((context) => ScreenA()),
-                      )),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('button'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  print('object');
+                },
+                onLongPress: () => print('object'),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.red,
                 ),
-              ))),
-      // debugShowCheckedModeBanner: false,
-      // initialRoute: '/',
-      // routes: {
-      //   '/': (context) => ScreenA(),
-      //   '/b': (context) => ScreenB(),
-      //   '/c': (context) => ScreenC()
-      // },
-      // home: Center(
-      //   child: ElevatedButton(
-      //     child: Text('go to A'),
-      //     onPressed: () => Navigator.push(
-      //         context,
-      //         MaterialPageRoute(
-      //           builder: ((context) => ScreenA()),
-      //         )),
-      //   ),
-      // ),
+                child: const Text(
+                  'hi',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('안녕');
+                },
+                child: Text('Elevated Button'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orangeAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 0.0,
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  print('OutlinedButton');
+                },
+                child: Text('OutlinedButton'),
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.green,
+                  side: const BorderSide(
+                    color: Colors.black87,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+              TextButton.icon(
+                onPressed: () {
+                  print('TextButton.icon');
+                },
+                icon: const Icon(
+                  Icons.home,
+                  size: 30.0,
+                  color: Colors.white,
+                ),
+                label: const Text('Go to Home'),
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(200, 50),
+                    backgroundColor: Colors.black87),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                buttonPadding: EdgeInsets.all(20.0),
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("hi"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('hi'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
