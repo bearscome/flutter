06f3305eb4b30c4764c49f3e3db26c9122c2a6fc
hslee <dlgytjdsla@naver.com>
2022-10-08 10:21:27 +0900

    dd

--- flutter_application_2/lib/onboarding.dart
+++ flutter_application_2/lib/onboarding.dart
@@ -46,30 +46,32 @@ class OnBoardingPage extends StatelessWidget {
       showSkipButton: true,
       skip: const Text('skip'),
       dotsDecorator: DotsDecorator(
-          color: Colors.cyan,
-          size: Size(10, 10),
-          activeSize: Size(22, 10),
-          activeShape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(24),
-          ),
-          activeColor: Colors.pink),
+        color: Colors.cyan,
+        size: const Size(10, 10),
+        activeSize: const Size(22, 10),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        activeColor: Colors.pink,
+      ),
       curve: Curves.bounceOut,
     );
   }
 
   PageDecoration getPageDecoration() {
     return const PageDecoration(
-        titleTextStyle: TextStyle(
-          fontSize: 28,
-          fontWeight: FontWeight.bold,
-        ),
-        bodyTextStyle: TextStyle(
-          fontSize: 18,
-          color: Colors.blue,
-        ),
-        imagePadding: EdgeInsets.only(
-          top: 40,
-        ),
-        pageColor: Colors.pink);
+      titleTextStyle: TextStyle(
+        fontSize: 28,
+        fontWeight: FontWeight.bold,
+      ),
+      bodyTextStyle: TextStyle(
+        fontSize: 18,
+        color: Colors.blue,
+      ),
+      imagePadding: EdgeInsets.only(
+        top: 40,
+      ),
+      pageColor: Colors.pink,
+    );
   }
 }
