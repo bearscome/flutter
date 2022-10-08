d4b265ce07cce2ef0a5ecf10604b56e111d06ebe
hslee <dlgytjdsla@naver.com>
2022-10-08 16:51:50 +0900

    Listtile

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/animal_page.dart';
+import 'package:flutter_application_1/model.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,145 +9,70 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: ListViewPage());
+    return const MaterialApp(home: _MypageState());
   }
 }
 
-class ListViewPage extends StatefulWidget {
-  const ListViewPage({super.key});
+class _MypageState extends StatefulWidget {
+  const _MypageState({super.key});
 
   @override
-  State<ListViewPage> createState() => _ListViewPageState();
+  State<_MypageState> createState() => __MypageStateState();
 }
 
-class _ListViewPageState extends State<ListViewPage> {
-  var titleList = ['a', 'b', 'c'];
-  var imgaeList = ['images/1.png', 'images/2.png', 'images/3.png'];
-  var description = [
-    '12121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121212',
-    '34',
-    '56'
+class __MypageStateState extends State<_MypageState> {
+  static List<String> animalName = [
+    'Bear',
+    'Camel',
+    'Deer',
   ];
 
-  void showPopup(context, title, image, desction) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return Dialog(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.7,
-            height: 380,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10), color: Colors.white),
-            child: Column(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.asset(
-                    image,
-                    width: 200,
-                    height: 200,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  title,
-                  style: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Text(
-                    desction,
-                    maxLines: 3,
-                    style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(Icons.close),
-                  label: const Text('close'),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
+  static List<String> animalImagePath = [
+    'images/1.png',
+    'images/2.png',
+    'images/3.png',
+  ];
+
+  static List<String> animalLocation = [
+    'KR',
+    'JP',
+    'CH',
+  ];
+
+  final List<Animal> animalData = List.generate(
+    animalLocation.length,
+    (index) => Animal(
+        animalName[index], animalImagePath[index], animalLocation[index]),
+  );
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width * 0.6;
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'ListView',
-          style: TextStyle(
-            color: Colors.grey,
-          ),
-        ),
+        title: const Text('Listview'),
       ),
       body: ListView.builder(
-        itemCount: titleList.length,
+        itemCount: animalData.length,
         itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              debugPrint(titleList[index]);
-              showPopup(context, titleList[index], imgaeList[index],
-                  description[index]);
-            },
-            child: Card(
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: Image.asset(
-                      imgaeList[index],
+          return Card(
+            child: ListTile(
+              title: Text(
+                animalData[index].name,
+              ),
+              leading: SizedBox(
+                width: 50,
+                height: 50,
+                child: Image.asset(animalData[index].imgPath),
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => AnimalPage(
+                      animal: animalData[index],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        Text(
-                          titleList[index],
-                          style: const TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        SizedBox(
-                          width: width,
-                          child: Text(
-                            description[index],
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.grey[500],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                );
+              },
             ),
           );
         },
