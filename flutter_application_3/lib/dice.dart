8bded2a8790f984c47bf50f08c78c77e9fd84139
hslee <dlgytjdsla@naver.com>
2022-10-09 11:24:28 +0900

    로그인 페이지

---
+++ flutter_application_3/lib/dice.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class Dice extends StatelessWidget {
+  const Dice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hi'),
+      ),
+    );
+  }
+}
--- flutter_application_3/lib/main.dart
+++ flutter_application_3/lib/main.dart
@@ -1,90 +1,141 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_application_3/dice.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Dice game',
+      home: LogIn(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class LogIn extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LogIn> createState() => _LogInState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void minus() {
-    setState(() {
-      _counter--;
-    });
-  }
+class _LogInState extends State<LogIn> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Log in'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
+        actions: <Widget>[
+          IconButton(icon: const Icon(Icons.search), onPressed: () {})
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    _incrementCounter();
-                  },
-                  child: Text('+'),
+      body: Builder(
+        builder: (context) {
+          return SingleChildScrollView(
+              child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 50),
+              ),
+              const Center(
+                child: Image(
+                  image: AssetImage('image/chef.gif'),
+                  width: 170,
+                  height: 190,
                 ),
-                ElevatedButton(
-                  onPressed: () => minus(),
-                  child: Text('-'),
+              ),
+              Form(
+                child: Theme(
+                  data: ThemeData(
+                    primaryColor: Colors.teal,
+                    inputDecorationTheme: const InputDecorationTheme(
+                      labelStyle: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.all(40),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: controller,
+                          decoration: const InputDecoration(
+                            labelText: 'Enter Dice',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        TextField(
+                          controller: controller2,
+                          decoration: const InputDecoration(
+                            labelText: 'Enter Password',
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            print(controller.text);
+                            debugPrint('hi');
+                            if (controller.text == 'asd' &&
+                                controller2.text == 'asd') {
+                              print('object');
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => Dice(),
+                                ),
+                              );
+                            } else if (controller.text != 'asd' &&
+                                controller2.text == 'asd') {
+                              showSnackBar(context, 'dice 철자가 일치하지 않습니다.');
+                            } else if (controller.text == 'asd' &&
+                                controller2.text != 'asd') {
+                              showSnackBar(context, '비밀번호가 일치하지 않습니다.');
+                            } else {
+                              showSnackBar(context, '철자와 비밀번호가 일치하지 않습니다.');
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orangeAccent,
+                              minimumSize: const Size(100, 50)),
+                          child: const Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ],
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+              ),
+            ],
+          ));
+        },
       ),
     );
   }
+}
+
+void showSnackBar(BuildContext context, String msg) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        msg,
+        textAlign: TextAlign.center,
+      ),
+      duration: const Duration(seconds: 2),
+      backgroundColor: Colors.blue,
+    ),
+  );
 }
