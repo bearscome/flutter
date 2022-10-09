793f673b4c29d3c6fdb7da000c5cc4d6875ff1e7
hslee <dlgytjdsla@naver.com>
2022-10-09 14:59:46 +0900

    login, randomDice

--- flutter_application_3/lib/main.dart
+++ flutter_application_3/lib/main.dart
@@ -37,90 +37,95 @@ class _LogInState extends State<LogIn> {
       ),
       body: Builder(
         builder: (context) {
-          return SingleChildScrollView(
+          return GestureDetector(
+            child: SingleChildScrollView(
               child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 50),
-              ),
-              const Center(
-                child: Image(
-                  image: AssetImage('image/chef.gif'),
-                  width: 170,
-                  height: 190,
-                ),
-              ),
-              Form(
-                child: Theme(
-                  data: ThemeData(
-                    primaryColor: Colors.teal,
-                    inputDecorationTheme: const InputDecorationTheme(
-                      labelStyle: TextStyle(
-                        color: Colors.teal,
-                        fontSize: 15,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 50),
+                  ),
+                  const Center(
+                    child: Image(
+                      image: AssetImage('image/chef.gif'),
+                      width: 170,
+                      height: 190,
+                    ),
+                  ),
+                  Form(
+                    child: Theme(
+                      data: ThemeData(
+                        primaryColor: Colors.teal,
+                        inputDecorationTheme: const InputDecorationTheme(
+                          labelStyle: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(40),
+                        child: Column(
+                          children: [
+                            TextField(
+                              // autofocus: true, // 자동으로 포커스가 이동하여 키보드가 노출된다.
+                              controller: controller,
+                              decoration: const InputDecoration(
+                                labelText: 'Enter Dice',
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            TextField(
+                              controller: controller2,
+                              decoration: const InputDecoration(
+                                labelText: 'Enter Password',
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true, // 암호화 텍스트
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                debugPrint('hi');
+                                if (controller.text == 'asd' &&
+                                    controller2.text == 'asd') {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => const Dice(),
+                                    ),
+                                  );
+                                } else if (controller.text != 'asd' &&
+                                    controller2.text == 'asd') {
+                                  showSnackBar(context, 'dice 철자가 일치하지 않습니다.');
+                                } else if (controller.text == 'asd' &&
+                                    controller2.text != 'asd') {
+                                  showSnackBar(context, '비밀번호가 일치하지 않습니다.');
+                                } else {
+                                  showSnackBar(context, '철자와 비밀번호가 일치하지 않습니다.');
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.orangeAccent,
+                                  minimumSize: const Size(100, 50)),
+                              child: const Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                                size: 35,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                  child: Container(
-                    padding: const EdgeInsets.all(40),
-                    child: Column(
-                      children: [
-                        TextField(
-                          controller: controller,
-                          decoration: const InputDecoration(
-                            labelText: 'Enter Dice',
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        TextField(
-                          controller: controller2,
-                          decoration: const InputDecoration(
-                            labelText: 'Enter Password',
-                          ),
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                        ),
-                        const SizedBox(
-                          height: 40,
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            print(controller.text);
-                            debugPrint('hi');
-                            if (controller.text == 'asd' &&
-                                controller2.text == 'asd') {
-                              print('object');
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => Dice(),
-                                ),
-                              );
-                            } else if (controller.text != 'asd' &&
-                                controller2.text == 'asd') {
-                              showSnackBar(context, 'dice 철자가 일치하지 않습니다.');
-                            } else if (controller.text == 'asd' &&
-                                controller2.text != 'asd') {
-                              showSnackBar(context, '비밀번호가 일치하지 않습니다.');
-                            } else {
-                              showSnackBar(context, '철자와 비밀번호가 일치하지 않습니다.');
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.orangeAccent,
-                              minimumSize: const Size(100, 50)),
-                          child: const Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
-                            size: 35,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                ],
               ),
-            ],
-          ));
+            ),
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+          );
         },
       ),
     );
