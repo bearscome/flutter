768156d859b1436debd021f013b19f74a55aeb55
hyosung <67565847+bearscome@users.noreply.github.com>
2022-11-18 14:17:37 +0900

    fcm
    testing

--- flutter_application_4/lib/main.dart
+++ flutter_application_4/lib/main.dart
@@ -1,11 +1,78 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_4/page/main.dart';
 import 'package:flutter_application_4/provider/deviceProvider.dart';
 import 'package:flutter_application_4/provider/loginProvider.dart';
 import 'package:flutter_application_4/provider/todoListProvider.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: true,
+    criticalAlert: true,
+    provisional: true,
+    sound: true,
+  );
+
+  // foreground에서의 푸시 알림 표시를 위한 알림 중요도 설정
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.max,
+  );
+
+  // foreground에서의 푸시 알림 표시를 위한 local notifications 설정
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  // // foreground에서의 푸시 알림 표시를 위한 local notifications 설정
+  await flutterLocalNotificationsPlugin.initialize(
+    InitializationSettings(
+        android: AndroidInitializationSettings('@mipmap/launcher_icon'),
+        iOS: DarwinInitializationSettings(
+            onDidReceiveLocalNotification: (id, title, body, payload) {
+          print('id, title, body, payload');
+        })),
+    onDidReceiveNotificationResponse: (details) {
+      print(details);
+    },
+    onDidReceiveBackgroundNotificationResponse: (details) {
+      print(details);
+    },
+  );
+
+  // foreground 푸시 알림 핸들링
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+
+    if (notification != null && android != null) {
+      flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channelDescription: channel.description,
+              icon: android.smallIcon,
+            ),
+          ));
+    }
+  });
+
   runApp(
     MultiProvider(
       providers: [
--- flutter_application_4/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_application_4/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,9 @@ import Foundation
 
 import audio_session
 import audioplayers_darwin
+import firebase_core
+import firebase_messaging
+import flutter_local_notifications
 import geolocator_apple
 import just_audio
 import path_provider_macos
@@ -15,6 +18,9 @@ import sqflite
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
